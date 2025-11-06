fb1d94f5837f65f596651b4b3e4840f4a2b2c9c2
Matthew Pullen <matthew.pullen@gmail.com>
2025-11-06 15:15:12 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:forui/forui.dart';
+
+void main() {
+  runApp(const Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    /// Try changing this and hot reloading the application.
+    ///
+    /// To create a custom theme:
+    /// ```shell
+    /// dart forui theme create [theme template].
+    /// ```
+    final theme = FThemes.zinc.dark;
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // TODO: replace with your application's supported locales.
+      supportedLocales: FLocalizations.supportedLocales,
+      // TODO: add your application's localizations delegates.
+      localizationsDelegates: const [...FLocalizations.localizationsDelegates],
+      builder: (_, child) => FTheme(data: theme, child: child!),
+      theme: theme.toApproximateMaterialTheme(),
+      // You can also replace FScaffold with Material Scaffold.
+      home: const FScaffold(
+        // TODO: replace with your widget.
+        child: Example(),
+      ),
+    );
+  }
+}
+
+class Example extends StatefulWidget {
+  const Example({super.key});
+
+  @override
+  State<Example> createState() => _ExampleState();
+}
+
+class _ExampleState extends State<Example> {
+  int _count = 0;
+
+  @override
+  Widget build(BuildContext context) => Center(
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      spacing: 10,
+      children: [
+        Text('Count: $_count'),
+        FButton(
+          onPress: () => setState(() => _count++),
+          suffix: const Icon(FIcons.chevronsUp),
+          child: const Text('Increase'),
+        ),
+      ],
+    ),
+  );
+}
